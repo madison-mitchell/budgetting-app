462fbe6e986304ba08e90aba34869920d1950fab
madison-mitchell <missmadisonrose@icloud.com>
2024-06-18 14:51:04 -0400

    Updated Navigation to allow for user to logout

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,9 +1,16 @@
 <template>
-    <nav>
-        <router-link v-if="!isLoggedIn" to="/">LOGIN</router-link>
-        <router-link v-else to="/profile">PROFILE</router-link> | <router-link to="/dashboard">DASHBOARD</router-link> | <router-link to="/bank-accounts">BANK ACCOUNTS</router-link> |
-        <router-link to="/transactions">TRANSACTIONS</router-link> |
-        <router-link to="/expenses">EXPENSES</router-link>
+    <nav class="flex justify-between p-4 text-gray-500 active:text-sky-500">
+        <div>
+            <router-link v-if="!isAuthenticated" to="/login" class="mr-4">LOGIN</router-link>
+            <router-link v-else to="/profile" class="mr-4">PROFILE</router-link>
+            <button v-if="isAuthenticated" @click="logout" class="mr-4 text-red-500">LOGOUT</button>
+        </div>
+
+        <div>
+            <router-link to="/dashboard" class="mr-4">DASHBOARD</router-link>
+            <router-link to="/bank-accounts" class="mr-4">BANK ACCOUNTS</router-link><router-link to="/transactions" class="mr-4">TRANSACTIONS</router-link>
+            <router-link to="/expenses" class="mr-4">EXPENSES</router-link>
+        </div>
     </nav>
 </template>
 
@@ -13,11 +20,20 @@ import authService from '../services/authService';
 export default {
     data() {
         return {
-            isLoggedIn: false,
+            isAuthenticated: !!authService.getCurrentUser(),
         };
     },
-    mounted() {
-        this.isLoggedIn = authService.getCurrentUser() !== null;
+    methods: {
+        logout() {
+            authService.logout();
+            this.isAuthenticated = false;
+            this.$router.push('/login');
+        },
+    },
+    watch: {
+        $route() {
+            this.isAuthenticated = !!authService.getCurrentUser();
+        },
     },
 };
 </script>
