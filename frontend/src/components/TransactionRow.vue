dfc1b0e6e2571cc4deae09249ffe31bee900c741
madison-mitchell <missmadisonrose@icloud.com>
2024-06-25 20:30:57 -0400

    Added icons to represent category names

--- frontend/src/components/TransactionRow.vue
+++ frontend/src/components/TransactionRow.vue
@@ -20,7 +20,9 @@
             </td>
             <td class="pl-4 py-4 whitespace-nowrap">{{ formatBalance(split.plannedAmount) }}</td>
             <td class="px-4 py-4 whitespace-nowrap flex items-center">
-                {{ split.categoryId.childCategory.name }} <i class="fa-solid fa-circle text-supersmall text-sky-500 ml-2" title="Split Transaction"></i>
+                <i :class="`fa ${getCategoryIcon(split.categoryId.childCategory.name)}`" class="mr-2"></i>
+                <!-- {{ split.categoryId.childCategory.name }} -->
+                <i class="fa-solid fa-circle text-supersmall text-sky-500 ml-2" title="Split Transaction"></i>
             </td>
         </tr>
     </template>
@@ -39,7 +41,10 @@
             <input type="checkbox" v-model="transaction.planned" @change="updateTransaction(transaction)" class="form-checkbox h-4 w-5 text-green-600 transition duration-150 ease-in-out" />
         </td>
         <td class="pl-4 py-4 whitespace-nowrap">{{ formatBalance(transaction.plannedAmount) }}</td>
-        <td class="px-4 py-4 whitespace-nowrap">{{ transaction.categoryId.childCategory.name }}</td>
+        <td class="px-4 py-4 whitespace-nowrap flex items-center">
+            <i :class="`fa ${getCategoryIcon(transaction.categoryId.childCategory.name)}`" class="mr-2"></i>
+            <!-- {{ transaction.categoryId.childCategory.name }} -->
+        </td>
     </tr>
 </template>
 
@@ -73,6 +78,62 @@ export default {
         },
         updateTransaction(row) {
             this.$emit('update-transaction', row);
+        },
+        getCategoryIcon(categoryName) {
+            const categoryIcons = {
+                // Dining
+                Restaurants: 'fa-utensils text-indigo-600',
+                Groceries: 'fa-shopping-basket text-indigo-600',
+                'Fast Food': 'fa-hamburger text-indigo-600',
+                // Education
+                'Online Courses': 'fa-laptop text-amber-600',
+                Books: 'fa-book text-amber-600',
+                Tuition: 'fa-graduation-cap text-amber-600',
+                Supplies: 'fa-pencil-alt text-amber-600',
+                // Entertainment
+                Hobbies: 'fa-paint-brush text-purple-600',
+                Subscription: 'fa-newspaper text-purple-600',
+                Concerts: 'fa-music text-purple-600',
+                Movies: 'fa-film text-purple-600',
+                Games: 'fa-gamepad text-purple-600',
+                // Flight
+                Materials: 'fa-box text-sky-600',
+                'Flight Time': 'fa-plane text-sky-600',
+                'Instructor Time': 'fa-chalkboard-teacher text-sky-600',
+                // Healthcare
+                Dental: 'fa-tooth text-cyan-600',
+                'Medical Bills': 'fa-file-medical text-cyan-600',
+                Prescriptions: 'fa-prescription-bottle-alt text-cyan-600',
+                Vision: 'fa-eye text-cyan-600',
+                'Health Insurance': 'fa-heartbeat text-cyan-600',
+                // Housing
+                Rent: 'fa-home text-orange-600',
+                //Income
+                Salary: 'fa-money-bill-wave text-green-600',
+                // Miscellaneous
+                Other: 'fa-ellipsis-h text-gray-600',
+                Donations: 'fa-hand-holding-heart text-gray-600',
+                // Personal Care
+                Spa: 'fa-spa text-teal-600',
+                Cosmetics: 'fa-makeup text-teal-600',
+                Haircuts: 'fa-cut text-teal-600',
+                'Gym Membership': 'fa-dumbbell text-teal-600',
+                // Shopping
+                Clothing: 'fa-tshirt text-rose-600',
+                Electronics: 'fa-tv text-rose-600',
+                'Home Goods': 'fa-couch text-rose-600',
+                Gifts: 'fa-gift text-rose-600',
+                Toys: 'fa-puzzle-piece text-rose-600',
+                // Split Transaction
+                'Split Transaction': 'fa-random text-slate-600',
+                // Transportation
+                'Car Insurance': 'fa-shield-alt text-red-600',
+                'Car Payment': 'fa-car text-red-600',
+                Fuel: 'fa-gas-pump text-red-600',
+                Repairs: 'fa-wrench text-red-600',
+                'Public Transport': 'fa-bus text-red-600',
+            };
+            return categoryIcons[categoryName] || 'fa-circle'; // Default icon if category not found
         },
     },
 };
