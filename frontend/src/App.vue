99c5b0af56cbc7ad1418b2fc12bbb3dcc0565c77
madison-mitchell <missmadisonrose@icloud.com>
2024-07-21 15:59:13 -0400

    Updated styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -30,12 +30,17 @@ export default {
 </script>
 
 <style>
+body {
+    background-color: #edf6f9;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    /* color: #2c3e50; */
+    /* background-color: #f5f5f5; */
 }
 
 nav {
